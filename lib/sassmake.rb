d75e21b7654ce14ed2a432c733d1d4cd9e58c38a
royels <docmentz@gmail.com>
2015-09-17 22:12:15 -0700

    added automatic imports

--- lib/sassmake.rb
+++ lib/sassmake.rb
@@ -5,17 +5,28 @@ require "thor"
 
 module Sassmake
   class Build < Thor
+
+
     desc 'build', 'builds directories for you'
     def build_directories
+      FileUtils.touch '_main.scss'
       for directory in Variables.get_files_and_directories.keys
         FileUtils.mkdir directory
+        module_name = directory + '/_' + Variables.get_module_name() + '.scss'
+        FileUtils.touch module_name
         for file in Variables.get_files_and_directories[directory]
-          FileUtils.touch directory + '/_' + file + '.scss'
+          file_name = directory + '/_' + file + '.scss'
+          FileUtils.touch file_name
+          File.open(module_name, "a+") {|f| f.puts "@import " + "\"" + file + "\";" }
         end
-        FileUtils.touch directory + '/_' + Variables.get_module_name() + '.scss'
+        File.open("_main.scss", "a+") {|f| f.puts "@import " + "\"" + directory + "/" + Variables.get_module_name() + "\";" }
       end
-      FileUtils.touch '_main.scss'
     end
+
+
+
+
+
     desc 'destroy' , 'destroys built sassmake directories'
     def destroy_directories
       for directory in Variables.get_files_and_directories.keys
@@ -23,5 +34,6 @@ module Sassmake
       end
       FileUtils.rm '_main.scss'
     end
+
   end
 end
