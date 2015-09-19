f24e015873f2bb8441e1567b24ac4580f950cc51
royels <docmentz@gmail.com>
2015-09-18 21:35:07 -0700

    changed page defaults

--- lib/sassmake/constant.rb
+++ lib/sassmake/constant.rb
@@ -2,8 +2,7 @@ module Variables
   @@files_and_directories = { "base" => ["base", "reset", "typography"] ,
                              "components" => ["media", "button", "carousel", "thumbnails"],
                              "layout" => ["grid", "header", "footer", "sidebar", "forms", "dropdown"],
-                             "pages" => ["home-index", "home-about", "home-contact"],
-                             "themes" => ["user", "admin"],
+                             "pages" => ["_page1", "_page2", "_page3"],
                              "utils" => ["variables", "mixins", "functions", "helpers"],
                              "vendors" => ["bootstrap"]
                            }
